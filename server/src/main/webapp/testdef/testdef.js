202d9955a496ed5c3a2c469617610ed088d1aae4
Michael Suzuki <michael.suzuki@gmail.com>
2014-05-07 17:02:16 +0100

    fix naming convention of csv file to testname-runname.csv, BENCH-267

--- server/src/main/webapp/testdef/testdef.js
+++ server/src/main/webapp/testdef/testdef.js
@@ -798,9 +798,8 @@
                     $scope.summary = {};
                     var path = $location.path();
                     var names = path.replace("/tests/", "").split("/");
-                    $scope.summary.testname = names[0];
+                    $scope.testname = names[0];
                     var runname = names[1];
-                    $scope.summary.result =[50,40];
 
                     TestRunService.getTestRunSummary({
                         id: names[0],
