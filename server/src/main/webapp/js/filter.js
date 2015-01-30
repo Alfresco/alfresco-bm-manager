412120ab0d60cf732ebb0beed0aafd1e17bcd07b
Paul Brodner <paul.brodner@gmail.com>
2015-01-30 12:29:41 +0200

    Client side filtering for all test properties
    * css/javascript filtering
    * hide/show only filtered property names
    * expand/collapse property details

---
+++ server/src/main/webapp/js/filter.js
@@ -0,0 +1,31 @@
+/* 
+Filter rows marked as "filtrable" based on filtre-value data searched in "filtre" input
+This will also expand and collapse the "test list-groups" in order to view all the filtered values
+*/ 
+
+angular.module('Filter', []);
+
+function TestProperties($scope) {
+    angular.element(document).ready(function () {
+        var properties = {
+        showAll : function (value) {
+          var sufix = (" ng-hide"===value) ? "" : " ng-hide";
+          var propertyGroups = document.getElementsByClassName("test list-group" + sufix);
+          for (var i=0, max=propertyGroups.length; i < max; i++) {
+            propertyGroups[i].className="test list-group" + value;
+          } 
+        }
+      }
+        var filterValue = document.getElementById('filter_value');
+        document.getElementById('filter').addEventListener('input', function() {
+      //this will get all property groups that are hiddend    
+      if (!this.value) {
+        filterValue.innerHTML = "";
+        properties.showAll(" ng-hide");
+        return;
+      }
+      filterValue.innerHTML = ".filtrable:not([filtre-value*=\"" + this.value.toLowerCase() + "\"]) { display: none; }";
+      properties.showAll("");
+      });
+    });
+};
