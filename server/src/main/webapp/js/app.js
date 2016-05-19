49dad989962874c4e8225a9025a8b31695e4a4af
Frank Becker <fkbecker@Franks-Bwana.fritz.box>
2016-05-19 11:43:17 +0200

    #121

--- server/src/main/webapp/js/app.js
+++ server/src/main/webapp/js/app.js
@@ -387,3 +387,29 @@ app.directive('ngFocus', [function() {
     }
   }
 }]);
+
+app.controller('TestPropertiesFilterCtrl', ['$scope', TestPropertiesFilterCtrl]);
+function TestPropertiesFilterCtrl($scope) { 
+			angular.element(document).ready(function () {
+				var properties = {
+						showAll : function (value) {
+							var sufix = (" ng-hide"===value) ? "" : " ng-hide";
+							var propertyGroups = document.getElementsByClassName("test list-group" + sufix);
+							for (var i=0, max=propertyGroups.length; i < max; i++) {
+								propertyGroups[i].className="test list-group" + value;
+							}
+						}
+				}
+				var filterValue = document.getElementById('filter_value');
+				document.getElementById('filter').addEventListener('input', function() {
+					//this will get all property groups that are hidden
+					if (!this.value) {
+						filterValue.innerHTML = "";
+						properties.showAll(" ng-hide");
+						return;
+					}
+					filterValue.innerHTML = ".filterable:not([filter-value*=\"" + this.value.toLowerCase() + "\"]) { display: none; }";
+					properties.showAll("");
+				});
+			});
+};
