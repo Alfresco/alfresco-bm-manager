3f8479fb693dff888d2b8672db7ed526a093f846
Andreea Nechifor <Andreea.Nechifor@ness.com>
2018-08-01 10:40:30 +0300

    Change Application

--- server/src/main/java/org/alfresco/bm/manager/Application.java
+++ server/src/main/java/org/alfresco/bm/manager/Application.java
@@ -2,8 +2,10 @@ package org.alfresco.bm.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource({ "classpath:config/spring/app-context.xml" })
 public class Application
 {
     public static void main(String[] args)
